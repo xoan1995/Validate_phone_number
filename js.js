b8f3359fdbb07d3c3cd9b09513b5675b2cd109d1
Xoan Nguyen <hoanglapnhue@.com>
2019-07-19 11:08:06 +0700

    demo

---
+++ js.js
@@ -0,0 +1,13 @@
+
+function validatePhoneNumber(){
+    let account = document.getElementById("phoneNumber").value;
+    regexp = /^[0-9]{10}$/;
+
+    if (regexp.test(account) && account.length==10) {
+            document.getElementById("kq").innerText = "Valid phone number" + "(" + " số điện thoại hợp lệ " + ")";
+
+    }
+    else {
+        document.getElementById("kq").innerText = "Invalid phone number"+"("+" số điện thoại không hợp lệ "+")";
+    }
+}
